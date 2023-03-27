86ab2967d949c011abe17c20c90e999bbf465901
gpucce <g.puccetti92@gmail.com>
2023-03-27 21:13:35 +0200

    add RN50 result

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 |  0 | mscoco_captions | selfsustain_ViT-B-32 | en         |                   0.126349 |                    0.1262 |
 |  1 | mscoco_captions | ViT-B-32             | en         |                   0.119792 |                    0.1238 |
 |  2 | mscoco_captions | selfsustain_RN50     | en         |                   0.322471 |                    0.3906 |
-|  3 | mscoco_captions | ViT-B-32             | en         |                   0.60036  |                    0.7646 |
-|  4 | mscoco_captions | coca_ViT-B-32        | en         |                   0.619832 |                    0.7814 |
+|  3 | mscoco_captions | RN50                 | en         |                   0.311116 |                    0.3644 |
+|  4 | mscoco_captions | ViT-B-32             | en         |                   0.60036  |                    0.7646 |
+|  5 | mscoco_captions | coca_ViT-B-32        | en         |                   0.619832 |                    0.7814 |
--- multi_experiments.sh
+++ multi_experiments.sh
@@ -19,22 +19,30 @@
 #     --pretrained ../open_clip/vit_test/2023_03_27-12_05_56-model_ViT-B-32-lr_0.0005-b_32-j_2-p_amp/checkpoints/epoch_21.pt \
 #     --output ./vit_results.json
 
+clip_benchmark eval --dataset mscoco_captions \
+    --dataset_root ../root/mscoco \
+    --task zeroshot_retrieval \
+    --model RN50 \
+    --pretrained ../open_clip/vit_test/2023_03_27-18_11_04-model_RN50-lr_0.0005-b_32-j_2-p_amp/checkpoints/epoch_21.pt \
+    --output ./rn50_results.json
+
 # clip_benchmark eval --dataset mscoco_captions \
 #     --dataset_root ../root/mscoco \
 #     --task zeroshot_retrieval \
 #     --model ViT-B-32 --output ./pretrained_vit_results.json
     
-clip_benchmark eval --dataset mscoco_captions \
-    --dataset_root ../root/mscoco \
-    --task zeroshot_retrieval \
-    --model coca_ViT-B-32 \
-    --pretrained laion2B-s13B-b90k \
-    --output ./pretrained_coca_results.json
+# clip_benchmark eval --dataset mscoco_captions \
+#     --dataset_root ../root/mscoco \
+#     --task zeroshot_retrieval \
+#     --model coca_ViT-B-32 \
+#     --pretrained laion2B-s13B-b90k \
+#     --output ./pretrained_coca_results.json
 
 clip_benchmark build \
     ./isti_results.json \
     ./vit_results.json \
     ./isti_rn50_results.json \
+    ./rn50_results.json \
     ./pretrained_vit_results.json \
     ./pretrained_coca_results.json \
     --output ./all_results.csv
