5a4fef5a59cc87fd10186c74bb73b464123d0c24
gpucce <g.puccetti92@gmail.com>
2023-03-27 18:19:47 +0200

    better md

--- convert2md.py
+++ convert2md.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-pd.read_csv("all_results.csv").loc[:, ["dataset", "model", "language", "image_retrieval_recall@5", "text_retrieval_recall@5"]].to_markdown("all_results.md")
+pd.read_csv("all_results.csv").loc[:, ["dataset", "model", "language", "image_retrieval_recall@5", "text_retrieval_recall@5"]].to_markdown("README.md")
